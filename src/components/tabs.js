7b3446c6ecf54d8269908cc128e312462cda8897
Tram Tran <tramtranx@gmail.com>
2020-10-10 10:33:00 +0700

    Update tabs.js

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
-import Tab from './Tab';
+import Tab from "../components/tab";
 
 class Tabs extends Component {
     static propTypes = {
