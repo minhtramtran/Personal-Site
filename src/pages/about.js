eb84c54430d66f01d346438a5c5b7a2face9be3d
Tram Tran <tramtranx@gmail.com>
2020-12-20 20:49:02 +0700

    Remove default photo in About

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
-import Img from 'gatsby-image'
 import Layout from "../components/layout"
 
 const About = ({ data: { about } }) => (
@@ -11,9 +10,6 @@ const About = ({ data: { about } }) => (
       <div className="sheet__inner">
         <h1 className="sheet__title">{about.title}</h1>
         <p className="sheet__lead">{about.subtitle}</p>
-        <div className="sheet__gallery">
-          <Img fluid={about.photo.fluid} />
-        </div>
         <div
           className="sheet__body"
           dangerouslySetInnerHTML={{
@@ -35,11 +31,6 @@ export const query = graphql`
       }
       title
       subtitle
-      photo {
-        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
-          ...GatsbyDatoCmsSizes
-        }
-      }
       bioNode {
         childMarkdownRemark {
           html
