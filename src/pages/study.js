0c5a759975149d3ae886f7ad70368cf117f1fdb7
Tram Tran <46833995+minhtramtran@users.noreply.github.com>
2022-11-14 09:02:24 +1100

    Create study.js

---
+++ src/pages/study.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { HelmetDatoCms } from 'gatsby-source-datocms'
+import Layout from "../components/layout"
+
+const Study = ({ data: { study } }) => (
+  <Layout>
+    <article className="sheet">
+      <HelmetDatoCms seo={study.seoMetaTags} />
+      <div className="sheet__inner">
+        <h1 className="sheet__title">{study.title}</h1>
+        <p className="sheet__byline">{study.subtitle}</p>
+        <div
+          className="sheet__body"
+          dangerouslySetInnerHTML={{
+            __html: research.bioNode.childMarkdownRemark.html,
+          }}
+        />
+      </div>
+    </article>
+  </Layout>
+)
+
+export default Study
+
+export const query = graphql`
+  query ResearchQuery {
+    research: datoCmsResearch {
+      seoMetaTags {
+        ...GatsbyDatoCmsSeoMetaTags
+      }
+      title
+      subtitle
+      bioNode {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`
